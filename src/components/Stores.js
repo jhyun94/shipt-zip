f26a03636c24d414ed7fe0bb3ad54848f0e0c3d7
jhyun94 <hyunjonathan@gmail.com>
2018-04-28 17:00:33 -0500

    display stores in order, and correct date

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -5,19 +5,34 @@ class Stores extends Component {
 
 	renderStores() {
 		const stores = _.sortBy(this.props.stores, 'name');
-
 		return (
 			_.map( stores,  (store) => {
 				return (
 					<div key={store.id} className="col-md-4">
 						<div className="text-center">
 							<h4>{store.name}</h4>
-							<p>{store.launch_date}</p>
+							<p>{this.checkDate(store.launch_date)}</p>
 						</div>
 					</div>
 				)
 			})
 		)
+	}
+
+	checkDate(date) {
+		const currentTime = new Date().getTime();
+		const launchTime = new Date(date).getTime();
+		
+		if (launchTime < currentTime) {
+			return 'Now Available';
+		}
+
+		const month = new Date(date).getUTCMonth() + 1;
+		const year = new Date(date).getUTCFullYear().toString().substr(2);
+
+		return `Coming ${month}/${year}`
+
+
 	}
 
 
