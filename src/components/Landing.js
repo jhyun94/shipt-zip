4e72f8437d4cb731e135ff8b01fb6758d00e4982
jhyun94 <hyunjonathan@gmail.com>
2018-04-28 12:45:44 -0500

    basic section in landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
 
+import step1 from '../images/step-1.png';
+import step2 from '../images/step-2.png';
+import step3 from '../images/step-3.png';
+
 
 class Landing extends Component {
 
 	render() {
 		return (
-			<div className="container">
-				<div className="bg-target">
-					<div className="text-center text-white">
-						<h1 className="">Target exclusive offer.</h1>
-						<h2>$49 membership (reg. $99) + $15 credit with qualifying purchase.*</h2>
-						<form className="form-inline justify-content-center">
-							<div className="form-group">
-								<input placeholder="Enter Zip code" className="form-control" type="text" />
-							</div>
-							<div className="form-group">
-								<button type="submit" className="btn btn-primary">Get Started</button>
-							</div>
-						</form>
-						<p>*Membership offer valid for new members only. Target order of $100 or more must be 
-						placed to qualify for $15 credit, which expires 30 days after qualifying order is placed 
-						and can be applied to any order from Shipt.</p>
+			<div>
+				<div className="container">
+					<div className="bg-target">
+						<div className="text-center text-white">
+							<h1 className="">Target exclusive offer.</h1>
+							<h2>$49 membership (reg. $99) + $15 credit with qualifying purchase.*</h2>
+							<form className="form-inline justify-content-center">
+								<div className="form-group">
+									<input placeholder="Enter Zip code" className="form-control" type="text" />
+								</div>
+								<div className="form-group">
+									<button type="submit" className="btn btn-primary">Get Started</button>
+								</div>
+							</form>
+							<p>*Membership offer valid for new members only. Target order of $100 or more must be 
+							placed to qualify for $15 credit, which expires 30 days after qualifying order is placed 
+							and can be applied to any order from Shipt.</p>
+						</div>
 					</div>
+				</div>
+
+				<div className="container">
+					<div className="row">
+						<div className="col-md-4">
+							<img src={step1} alt="step 1" className="img-fluid"/>
+							<h4>Scroll through the aisles.</h4>
+							<p>Create an order from your local store in our app or
+								on our website.
+							</p>
+
+						</div>
+
+						<div className="col-md-4">
+							<img src={step2} alt="step 2" className="img-fluid"/>
+							<h4>Our shoppers work their magic.</h4>
+							<p>Connect with your shopper and get live updates from
+								the aisles.
+							</p>
+						</div>
+
+						<div className="col-md-4">
+							<img src={step3} alt="step 3" className="img-fluid"/>
+							<h4>We deliver your groceries.</h4>
+							<p>Schedule a delivery, and your order will arrive in
+								as soon as 1 hour.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="text-center my-4">
+					<button className="btn btn-primary btn-lg">Get Started</button>
 				</div>
 			</div>
 		)
