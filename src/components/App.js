03fa72c113c534d9670798dee119b88988e6ba06
jhyun94 <hyunjonathan@gmail.com>
2018-04-27 23:44:26 -0500

    components boilerplate, basic promo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import '../App.css';
+
+import Promotional from './Promotional';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div>Hi there</div>
+        <Promotional />
       <BrowserRouter>
         <div>
         </div>
---
+++ src/components/Promotional.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Promotional = () => {
+
+	return (
+		<div className="text-center bg-gray">
+			<h4 class="text-white">get $50 off + $15 credit*</h4>
+		</div>
+	)
+}
+
+export default Promotional;
