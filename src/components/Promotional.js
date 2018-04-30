fda680b4f3b87d00e6c668a147d57dee5150694d
jhyun94 <hyunjonathan@gmail.com>
2018-04-29 22:36:04 -0500

    styled promo to match design more closely

--- src/components/Promotional.js
+++ src/components/Promotional.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Promotional = () => {
 
 	return (
-		<div className="text-center bg-gray py-1">
-			<p className="text-white text-uppercase mb-0"><small>get $50 off + $15 credit*</small></p>
+		<div className="bg-gray promo">
+			<a href="/"><p>get $50 off + $15 credit*</p></a>
 		</div>
 	)
 }
